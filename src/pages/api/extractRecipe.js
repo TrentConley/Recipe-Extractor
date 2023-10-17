9e54c646322c44dec8143f7f09cd3acfbd4b5575
Trent Conley <trentconley@gmail.com>
2023-10-16 20:10:19 -0400

    extracting text from image

--- src/pages/api/extractRecipe.js
+++ src/pages/api/extractRecipe.js
@@ -1,17 +1,45 @@
 import axios from 'axios';
+import cheerio from 'cheerio';
+
+const fetchTextFromWebsite = async (url) => {
+    try {
+        // Fetch the website content
+        const { data } = await axios.get(url);
+
+        // Initialize cheerio
+        const $ = cheerio.load(data);
+
+        // Extract text from the website
+        const textArray = [];
+        $('body').find('*').each((index, element) => {
+            const text = $(element).text().trim();
+            let i = 0;
+            if (text) {
+                if (i < 2) {
+                    console.log(text);
+                }
+                i++;
+                textArray.push(text);
+            }
+        });
+
+        const allText = textArray.join(' '); // Concatenate all text nodes into a single string
+        return allText;
+
+    } catch (error) {
+        console.error(`Error fetching text from ${url}: `, error);
+        return null;
+    }
+};
+
 
 export default async function handler(req, res) {
     const { url } = req.body;
 
     try {
-        // Use axios and cheerio for web scraping
-        const response = await axios.get(url);
-        const $ = cheerio.load(response.data);
-        let websiteContent = '';
-        $('body').each((i, element) => {
-            websiteContent += $(element).text();
-        });
-
+        console.log(`url is ${url}!`)
+        let websiteContent = await fetchTextFromWebsite(url);
+        console.log(websiteContent);
         // Call OpenAI API to extract the recipe
         const openaiRes = await axios.post(
             'https://api.openai.com/v1/chat/completions',
